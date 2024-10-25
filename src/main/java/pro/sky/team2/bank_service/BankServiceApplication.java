8d2c7e35e236f3c61e9854339ff5b2792cbb4c91
REIGaruki <gyakujo@mail.ru>
2024-10-25 22:29:29 +0300

    Final version

---
+++ src/main/java/pro/sky/team2/bank_service/BankServiceApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.team2.bank_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankServiceApplication.class, args);
+	}
+
+}
