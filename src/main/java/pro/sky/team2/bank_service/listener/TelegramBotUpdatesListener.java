0b7e4a64d205383d20ede6a4d335267dbd52e825
REIGaruki <gyakujo@mail.ru>
2024-10-29 15:58:37 +0300

    Telegram Bot

---
+++ src/main/java/pro/sky/team2/bank_service/listener/TelegramBotUpdatesListener.java
@@ -0,0 +1,39 @@
+package pro.sky.team2.bank_service.listener;
+
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Update;
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TelegramBotUpdatesListener implements UpdatesListener{
+
+    private final Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
+
+    @Autowired
+    private TelegramBot telegramBot;
+
+    @PostConstruct
+    public void init() {
+        telegramBot.setUpdatesListener(this);
+    }
+
+    @Override
+    public int process(List<Update> updates) {
+        try {
+            updates.forEach(update -> {
+                logger.info("Processing update succeeded");
+            });
+        } catch (Exception e) {
+            logger.error("Processing update failed: {}", e);
+        } finally {
+            return UpdatesListener.CONFIRMED_UPDATES_ALL;
+        }
+    }
+}
